fb7641c90e528de634c88668511c87205f5e953d
Alan-N-Pereira <itsalanpereira@gmail.com>
2021-11-02 22:51:57 +0530

    Home Page Styled

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,11 +3,12 @@ import './header.css';
 export default function Header() {
 	return (
 		<div className="header">
+			<div className="headerCircle"></div>
 			<div className="headerTitles">
 				<span className="headerTitleSm">React & Node</span>
 				<span className="headerTitleLg">Blog</span>
+				<button className="headerBtn" >POSTS</button>
 			</div>
-			<img className="headerImg" src="/images/header-image.jpg" alt="" srcset="" />
 		</div>
 	);
 }
